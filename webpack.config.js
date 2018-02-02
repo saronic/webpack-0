cbff007e3909c1e8ead3619a77801d424a051ab2
lsx office lenovo <lsx@fake.com>
2018-02-02 10:05:24 +0800

    add url-loader & image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,12 +19,23 @@ module.exports = {
                     use: 'css-loader'
                 }),
                 test: /\.css$/
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader:'url-loader',
+                        options: {limit: 40000}
+                    },
+                    'image-webpack-loader'
+                ]
+
             }
         ]
     },
     plugins: [
         new ExtractTextPlugin({
-            filename: 'css/initial.[name].[hash:8].css',
+            filename: 'css/initial.[name].css',
             allChunks: true
         })
     ]
